fcc7a0528b33bdb56b5d26d538dc423e5d3b21cc
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-10-17 09:23:29 -0400

    er

--- dom.js
+++ dom.js
@@ -4,7 +4,17 @@ const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".character-name"),
     h2s: document.querySelectorAll("h2"),
+    title: document.querySelector(".title"),
+    random: document.querySelector(".random"),
 }
+
+function titleedit(headin, bruh) {
+    headin.style.fontSize = "50px";
+    headin.style.backgroundColor = "green";
+    bruh.innerHTML = "This is now something";
+}
+
+titleedit(DOMSelectors.title, DOMSelectors.random);
 
 DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
