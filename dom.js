23ea02310b5867bdfeb2b5855a1c7f61495db90f
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-10-24 09:24:34 -0400

    sd

--- dom.js
+++ dom.js
@@ -1,57 +1,47 @@
 /*const form = document.querySelector("#form");
 console.log(form)*/
 const DOMSelectors = {
-    button: document.getElementById("btn"),
+    button: document.getElementById("submit"),
     form: document.querySelector("#form"),
-    firstName: document.querySelector(".character-name"),
-    h2s: document.querySelectorAll("h2"),
     title: document.querySelector(".title"),
-    random: document.querySelector(".random"),
-    points:document.querySelectorAll(".point"),
 }
 
-function makeAlbum() {
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach(
+        (el) => (el.textContent = DOMSelectors.firstName.value)
+    );
+    function clearbox() [
+        DOM
+    ]
+})
 
-}
-function addCard(Album) {
+
     //insertAdjacentHTML() look up
     //backticks allow u to input a string and insert variable in adjacent
     // html using ${} ex(Album.name) use .value after domselector variable name
-}
 
-function titleedit(headin, bruh) {
-    headin.style.fontSize = "50px";
-    headin.style.backgroundColor = "green";
-    bruh.textContent = "This is now something";
-}
 
-DOMSelectors.button.addEventListener("click", function(){
+
+//DOMSelectors.input.value = ""; makes the input box clear
+
+/*DOMSelectors.button.addEventListener("click", function(){
     titleedit(DOMSelectors.title, DOMSelectors.random)
     /*const album = makealbum()
     addCard()
     clearfields()
-    addremovebuttons*/
-})
+    addremovebuttons
+})*/
 
 
-function changelist() {
+/*function changelist() {
     let pointbruh = 2;
     DOMSelectors.points.forEach((poin) => {
         poin.addEventListener("click", function() {
             poin.textContent = `hello ${pointbruh}`;
             pointbruh++;
         });
-    });
-}
-
-changelist(DOMSelectors.points);
-
-/*DOMSelectors.form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2s.forEach(
-        (el) => (el.textContent = DOMSelectors.firstName.value)
-    );
-})*/
+    });*/
 
 //console.log(DOMSelectors.h2s);
